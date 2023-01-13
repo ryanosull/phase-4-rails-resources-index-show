fcb5fdd4f3b2846de46c897e4a701fac7bde88ef
Ryan <wrosullivan88@gmail.com>
2023-01-12 20:25:15 -0600

    commit tree 7cf6b8574399066f61ff1964f750d1f044abc7b3

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,18 @@
 class BirdsController < ApplicationController
+
+    #get
+    def index
+        render json: Bird.all
+    end
+
+    #get/id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "Birds aren't real"}, status: :not_found
+        end
+    end
+
 end

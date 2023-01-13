e4b24c113f1825772706e9f6b1ea492b911b185b
Ryan <wrosullivan88@gmail.com>
2023-01-12 20:20:35 -0600

    clearing new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds', to: 'bords#show'
+
+  resources :birds, only: [:index, :show]
+
 end
